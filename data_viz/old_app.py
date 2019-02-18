69dec4283fa51c50ef742e4e9f7e4841ff07aa26
Carlos Soriano <soriano.carlitos@gmail.com>
2019-02-18 16:26:21 -0600

    app update

--- data_viz/old_app.py
+++ data_viz/old_app.py
@@ -47,6 +47,32 @@ def asmr_channels():
         datum['avg_views_subscriber'] = round(datum['views']/datum['subs'])
         datum['date_created'] = datum['date_created'].isoformat()
 
+        # video_stats_dict = {}
+        # for channel in response:
+        #     video_stats_dict[channel] = {}
+        #     durlist = []
+        #     for dur in channel['duration']:
+        #         durlist.append(dur)
+        #     video_stats_dict[channel]['total_duration'] = sum(durlist)
+                
+
+
+        # datum['total_duration'] = sum([video['duration'] for video in datum['videos']])
+        # datum['total_comments'] = sum([video['comments'] for video in datum['videos']])
+        # datum['total_likes'] = sum([video['likes'] for video in datum['videos']])
+        # datum['total_dislikes'] = sum([video['dislikes'] for video in datum['videos']])
+        # datum['average_duration'] = datum['total_duration'] / datum['uploads']
+        # datum['average_comments'] = datum['total_comments'] / datum['uploads']
+        # datum['average_likes'] = datum['total_likes'] / datum['uploads']
+        # datum['average_dislikes'] = datum['total_dislikes'] / datum['uploads']
+        # datum['like_ratio'] = datum['total_likes'] / datum['total_dislikes']
+        # datum =  {
+        #     'channel_name': datum['channel_name'], 'channel_type': datum['channel_type'],'uploads': datum['uploads'], 'subs': datum['subs'], 
+        #     'views': datum['views'], 'date_created': datum['date_created'], 'channel_age': datum['channel_age'], 'upload_frequency': datum['upload_frequency'],
+        #     'avg_views_video':  datum['avg_views_video'], 'avg_views_subscriber': datum['avg_views_subscriber'], 'country': datum['country'], 'gender': datum['gender'], 
+        #     'birthyear': datum['birthyear'], 'twitter': datum['twitter'], 'instagram': datum['instagram'], 'twitch': datum['twitch'], 'facebook': datum['facebook'],  
+        #     'channel_id': datum['channel_id'], 'time_series': datum['time_series']
+        #           }
     return jsonify(asmr_data)
 
 @app.route("/table")
@@ -57,6 +83,16 @@ def table():
 def youtube250():
     return render_template("Top250Index.html")
 
+@app.route("/linedata")
+def linedata():
+    # return jsonify({'date':['Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug'],
+    #                 'miles':[10,7,15,17,12,14,15]})
+    dates = ['Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug']
+    miles = [10,7,15,17,12,14,15]
+    data = []
+    for i, _ in enumerate(dates):
+        data.append({'date': dates[i], 'miles': miles[i]})
+    return jsonify(data)
 @app.route("/line")
 def line():
     return render_template("line.html")
