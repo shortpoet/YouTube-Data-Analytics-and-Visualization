023bc19a272d4db5a44abfcace14ac369c596b01
Austen M <amanser1@gmail.com>
2019-02-20 17:08:26 -0600

    map updates

--- data_viz/static/js/entertainmentLogic.js
+++ data_viz/static/js/entertainmentLogic.js
@@ -8,7 +8,7 @@ var map = L.map("map", {
 L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
   maxZoom: 18,
-  id: "mapbox.streets-satellite",
+  id: "mapbox.streets",
   accessToken: API_KEY
 }).addTo(map);
 
